4a4232476c459109a0b8e321d4a9ffdc8e23b775
Simon Kötting <simon.koetting@elastic.co>
2026-04-20 17:45:29 +0200

    move tamper tests to own space

--- internal/fleet/agentpolicy/testdata/TestAccResourceAgentPolicyTamperProtection/step3/main.tf
+++ internal/fleet/agentpolicy/testdata/TestAccResourceAgentPolicyTamperProtection/step3/main.tf
@@ -3,6 +3,12 @@ provider "elasticstack" {
   kibana {}
 }
 
+resource "elasticstack_kibana_space" "tamper_space" {
+  space_id    = var.space_id
+  name        = "Tamper Protection ${var.space_id}"
+  description = "Isolated space for tamper protection acceptance test"
+}
+
 resource "elasticstack_fleet_agent_policy" "test_policy" {
   name            = var.policy_name
   namespace       = "default"
@@ -10,7 +16,9 @@ resource "elasticstack_fleet_agent_policy" "test_policy" {
   monitor_logs    = true
   monitor_metrics = false
   skip_destroy    = false
-  is_protected    = true
+  space_ids       = [var.space_id]
+
+  depends_on = [elasticstack_kibana_space.tamper_space]
 }
 
 resource "elasticstack_fleet_elastic_defend_integration_policy" "test" {
